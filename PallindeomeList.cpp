f6a6a3626260be8ff39394466fbb28f3f3858e2e
atharvanaik1870 <55330511+atharvanaik1870@users.noreply.github.com>
2021-05-15 15:30:56 +0530

    c++ palindrome list
    program to check palindrome list using stack

---
+++ PallindeomeList.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack <int> s;
+        ListNode* curr=head;
+        while(curr){
+            s.push(curr->val);
+            curr=curr->next;
+        }
+        
+        curr=head;
+        while (curr) {
+            if( curr->val != s.top()){
+                return false;
+            }
+            else{
+                s.pop();
+                curr = curr->next;
+            }
+        }
+        
+        return true;
+    }
+    };
