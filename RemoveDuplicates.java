84fc061c593b52a004839a0240d051418259346f
atharvanaik1870 <55330511+atharvanaik1870@users.noreply.github.com>
2021-06-04 16:17:30 +0530

    remove duplicates using stack
    if the given string has adjacent duplicate letters then remove both letters and return a new string.

---
+++ RemoveDuplicates.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        st.push(s.charAt(0));
+        for(int i=1;i<s.length();i++){
+            if(st.size()>0 && st.peek()==s.charAt(i)){
+                st.pop();
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+        String str="";
+        for(int i=0;i<st.size();i++){
+            str+=Character.toString(st.get(i));
+        }
+        
+        return str;
+        
+    }
+}
