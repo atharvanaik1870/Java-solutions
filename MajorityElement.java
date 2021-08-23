4a509f56782bb5597782e4283ed3997a0d98fcbd
atharvanaik1870 <55330511+atharvanaik1870@users.noreply.github.com>
2021-08-23 17:42:09 +0530

    majority element in unsorted array
    majority element in unsorted array using hashmap

--- MajorityElement.java
+++ MajorityElement.java
@@ -4,32 +4,23 @@ class test{
 	public static void main(String[] args) {
 		int arr[]={3, 3, 4, 2, 4, 4, 2, 4, 4};
 		int n = arr.length;
-		//Arrays.sort(arr);
-		int maxcount=0,index=-1;
+		HashMap<Integer,Integer> m = new HashMap<Integer,Integer>();
 		for(int i=0;i<n;i++)
 		{
-			int count=0;
-			for(int j=0;j<n;j++)
+			if(m.containsKey(arr[i]))
 			{
-				if(arr[i]==arr[j])
+				int count= m.get(arr[i])+1;
+				if(count > n/2)
 				{
-					count++;
-
+					System.out.println("majoroty element is = " +arr[i]);
 				}
+				else
+					m.put(arr[i],count);
 			}
-			if(maxcount<count)
-			{
-				maxcount=count;
-				index=i;
-			}
+			else
+				m.put(arr[i],1);
 		}
-		if(maxcount > n/2)
-			System.out.println("majority element is = "+ arr[index]);
-		else
-			System.out.println("no majority elemeny..");
-
-
-             
+		System.out.println("No majority element..");      
     }
 
 }
