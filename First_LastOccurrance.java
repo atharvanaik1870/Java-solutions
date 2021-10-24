e90cc223dc1eda5dbfd9d83619a5316cdd7b1085
atharvanaik1870 <55330511+atharvanaik1870@users.noreply.github.com>
2021-10-24 17:20:16 +0530

    added 2nd approach with log n time complexity

--- First_LastOccurrance.java
+++ First_LastOccurrance.java
@@ -32,3 +32,42 @@ class test{
     }
 
 }
+//Approach 2 
+//Time complexity = O(log n)
+class Solution {
+    public int[] searchRange(int[] arr, int target) {
+        
+        int start = search(arr, target, true);
+        int end = search(arr, target, false);
+        int a[] = { -1, -1 };
+
+        a[0] = start;
+        a[1] = end;
+
+        return a;
+    }
+    
+    public static int search(int arr[], int target, boolean searchfirst) {
+        int ans = -1;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                if (searchfirst) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
